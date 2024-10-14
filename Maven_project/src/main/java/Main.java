3e2af99ba3911774c367a41fcbcdab7c38831825
Valery <kharkov.valery@gmail.com>
2024-10-14 22:23:02 +0300

    Получение свойства элемента из раздела DevTools → Elements → Properties

--- Maven_project/src/main/java/Main.java
+++ Maven_project/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -9,6 +11,12 @@ public class Main {
         // Создание сеанса. Сеанс создается автоматически путем инициализации нового объекта класса Driver
         WebDriver driver = new ChromeDriver();
         // Запуск браузера
-        driver.get("https://ya.ru");
+        driver.get("https://www.google.com/");
+
+    // Поиск определённого элемента
+    WebElement element = driver.findElement(By.xpath("//img[contains(@alt,'Google')]"));
+    // Получение свойства элемента из раздела DevTools → Elements → Properties
+    String elementProperty = element.getAttribute("currentSrc"); // "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png"
+    System.out.println(elementProperty);
     }
 }
